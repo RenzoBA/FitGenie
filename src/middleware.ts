90785311d0b4ee2574be586107d8a73ae1513680
renzo <renzobocanegra.a@gmail.com>
2023-11-11 16:28:51 -0500

    NextRequest type added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { rateLimiter } from "./lib/rate-limiter";
 
 export async function middleware(req: NextRequest) {
