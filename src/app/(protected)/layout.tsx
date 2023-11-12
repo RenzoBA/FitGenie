a5d6ae2d755fc7050ece5659d5a38b6caf8ab645
renzo <renzobocanegra.a@gmail.com>
2023-11-12 10:20:50 -0500

    validate user session from server instead client

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,26 @@
-import ProtectedProvider from "@/components/ProtectedProvider";
+import Alert from "@/components/Alert";
+import { getAuthSession } from "@/lib/auth";
 import { ReactNode } from "react";
 
 interface layoutProps {
   children: ReactNode;
 }
 
-const layout = ({ children }: layoutProps) => {
-  return <ProtectedProvider>{children}</ProtectedProvider>;
+const layout = async ({ children }: layoutProps) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return (
+      <div className="h-screen flex justify-center items-center px-8">
+        <Alert
+          label="FitGenie"
+          description="Welcome back! Sign in to access personalized content and features."
+        />
+      </div>
+    );
+  }
+
+  return <div>{children}</div>;
 };
 
 export default layout;
