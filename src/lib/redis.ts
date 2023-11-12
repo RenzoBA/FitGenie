3747cf8d190373627d4cbfbfbd3f29141ac81d5e
renzo <renzobocanegra.a@gmail.com>
2023-11-11 22:18:12 -0500

    env variables renamed

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
-import { Redis } from '@upstash/redis'
+import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: process.env.REDIS_URL!,
-  token: process.env.REDIS_SECRET!,
-})
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
