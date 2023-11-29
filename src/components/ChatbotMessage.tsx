296e609c666d4d7b42696b38c7d180540d172c9b
renzo <renzobocanegra.a@gmail.com>
2023-11-29 18:30:20 -0500

    message margin removed

--- src/components/ChatbotMessage.tsx
+++ src/components/ChatbotMessage.tsx
@@ -51,7 +51,7 @@ const ChatbotMessage: FC<ChatbotMessageProps> = ({ message }) => {
       >
         <div
           className={cn(
-            "flex flex-col gap-2 text-sm max-w-[90%] mx-2 overflow-x-hidden rounded-t-lg p-3",
+            "flex flex-col gap-2 text-sm max-w-[90%] overflow-x-hidden rounded-t-lg p-3",
             {
               "bg-foreground text-secondary rounded-l-lg text-right":
                 message.isUserMessage,
