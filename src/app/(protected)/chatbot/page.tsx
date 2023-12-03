71a4d01c34d58c533454f02b5aecb121c6d8e4c3
renzo <renzobocanegra.a@gmail.com>
2023-12-02 20:31:49 -0500

    page title renamed

--- src/app/(protected)/chatbot/page.tsx
+++ src/app/(protected)/chatbot/page.tsx
@@ -5,7 +5,7 @@ const page = async () => {
   return (
     <div className="absolute inset-x-0 py-36 flex flex-col justify-start items-center text-center px-10">
       <div>
-        <h1 className="text-6xl font-black text-primary">CHATBOT</h1>
+        <h1 className="text-6xl font-black text-primary">CHAT</h1>
       </div>
       <div className="text-justify text-muted-foreground mt-3 leading-relaxed space-y-5 max-w-7xl w-full">
         <h3 className="font-light text-muted-foreground uppercase tracking-widest text-center">
